da256f298c41e2530d9933c642885e5065336a0d
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-04-22 00:26:38 -0400

    added function to done button

--- to-do-list/src/components/listItem.js
+++ to-do-list/src/components/listItem.js
@@ -1,8 +1,11 @@
+import {useState} from 'react'
 const ListItem = (props) => {
+
+    const [color, setColor] = useState('')
     return (
         <div className="listItemContainer">
             <div className="item" key = {props.i}>{props.note}</div>
-            <button className="doneButton">done</button>
+            <button className={`doneButton ${color}`} onClick={() => { setColor('green') }}>done</button>
         </div>
     )
 }
